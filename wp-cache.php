85ab8d696cae5ee75fe3f1d7a3668a2e81a41447
Rafik Israyelyan <israelyan.rafik@gmail.com>
2020-03-24 19:51:01 +0400

    Added method to get admin toolbar menu item configuration

--- wp-cache.php
+++ wp-cache.php
@@ -3821,41 +3821,72 @@ function supercache_admin_bar_render() {
 }
 
 /**
+ * Get admin bar "Delete Cache" menu item configuration
+* @return array
+ */
+function wpsc_get_admin_bar_item() {
+    $node = [];
+	if ( function_exists( 'current_user_can' ) && is_user_logged_in() ) {
+		global $wp_cache_request_uri;
+        if ( ( is_singular() || is_archive() || is_front_page() || is_search() || is_home() ) && current_user_can(  'delete_others_posts' ) ) {
+            $site_regex = preg_quote( rtrim( (string) parse_url( get_option( 'home' ), PHP_URL_PATH ), '/' ), '`' );
+            $req_uri    = preg_replace( '/[ <>\'\"\r\n\t\(\)]/u', '', $wp_cache_request_uri );
+            $path       = preg_replace( '`^' . $site_regex . '`', '', $req_uri );
+
+            $node = array(
+                'parent' => false,
+                'id' => 'delete-cache',
+                'title' => __( 'Delete Cache', 'wp-super-cache' ),
+                'meta' => array( 'title' => __( 'Delete cache of the current page', 'wp-super-cache' ) ),
+
+                /**
+                 * Modified by @aaron (@diazoxide)
+                 */
+                'href' => wp_nonce_url(
+                    add_query_arg(
+                        [
+                            'action' => 'delcachepage',
+                            'path' => wp_supercache_get_uri_cache_dir($path),
+                        ],
+                        admin_url('index.php')
+                    ),
+                    'delete-cache'
+                )
+            );
+        } elseif ( is_admin() && wpsupercache_site_admin() && current_user_can( 'manage_options' ) ) {
+            $node = array(
+                'parent' => false,
+                'id' => 'delete-cache',
+                'title' => __( 'Delete Cache', 'wp-super-cache' ),
+                'meta' => array( 'title' => __( 'Delete Super Cache cached files', 'wp-super-cache' ) ),
+                'href' => wp_nonce_url(
+                    add_query_arg(
+                        [
+                            'page' => 'wpsupercache',
+                            'tab' => 'contents',
+                            'wp_delete_cache' => 1
+                        ],
+                        admin_url('options-general.php')
+                    ),
+                    'wp-cache'
+                )
+            );
+        }
+	}
+
+	return $node;
+}
+
+/**
  * Adds "Delete Cache" button in WP Admin Bar.
  */
 function wpsc_admin_bar_render( $wp_admin_bar ) {
-    global $wp_cache_request_uri;
-	if ( ! function_exists( 'current_user_can' ) || ! is_user_logged_in() ) {
-		return false;
-	}
-
-	if ( ( is_singular() || is_archive() || is_front_page() || is_search() || is_home() ) && current_user_can(  'delete_others_posts' ) ) {
-		$site_regex = preg_quote( rtrim( (string) parse_url( get_option( 'home' ), PHP_URL_PATH ), '/' ), '`' );
-		$req_uri    = preg_replace( '/[ <>\'\"\r\n\t\(\)]/u', '', $wp_cache_request_uri );
-		$path       = preg_replace( '`^' . $site_regex . '`', '', $req_uri );
-
-		$wp_admin_bar->add_menu( array(
-					'parent' => '',
-					'id' => 'delete-cache',
-					'title' => __( 'Delete Cache', 'wp-super-cache' ),
-				    'meta' => array( 'title' => __( 'Delete cache of the current page', 'wp-super-cache' ) ),
-					
-					/**
-					 * Modified by @aaron (@diazoxide)
-					 * */
-					'href' => wp_nonce_url( admin_url( 'index.php?action=delcachepage&path=' . wp_supercache_get_uri_cache_dir( $path) ), 'delete-cache' )
-					) );
-	}
-
-	if ( is_admin() && wpsupercache_site_admin() && current_user_can( 'manage_options' ) ) {
-		$wp_admin_bar->add_menu( array(
-					'parent' => '',
-					'id' => 'delete-cache',
-					'title' => __( 'Delete Cache', 'wp-super-cache' ),
-					'meta' => array( 'title' => __( 'Delete Super Cache cached files', 'wp-super-cache' ) ),
-					'href' => wp_nonce_url( admin_url( 'options-general.php?page=wpsupercache&tab=contents&wp_delete_cache=1' ), 'wp-cache' )
-					) );
-	}
+    global $wp_admin_bar;
+    /** @var $wp_admin_bar WP_Admin_Bar */
+    $node = wpsc_get_admin_bar_item();
+    if(!empty($node)) {
+        $wp_admin_bar->add_menu($node);
+    }
 }
 add_action( 'admin_bar_menu', 'wpsc_admin_bar_render', 99 );
 
